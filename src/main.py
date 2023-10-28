ced85ca75511b00d8e298d9c875322436feeacc6
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2023-10-28 11:41:52 +0300

    updated comments

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
+# added comments
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = ""):  # Adjust the default value to an empty string
+def read_item(item_id: int, q: str = ""):  
     return {"item_id": item_id, "q": q}
