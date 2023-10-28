559a09a6b6fbb06a235c9a461c6d61d2afcd8e4b
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2023-10-28 13:22:19 +0300

    updated comments

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 # added comments
+# changed comments
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = ""):  
     return {"item_id": item_id, "q": q}
